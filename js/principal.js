252cca3191e2ad1c8dd2e414f98de40b45649990
PiorBR1 <160504825+PiorBR1@users.noreply.github.com>
2024-11-04 11:52:03 -0300

    carga_adicionar-dados

--- js/principal.js
+++ js/principal.js
@@ -18,20 +18,24 @@ for (var i = 0; i < pacientes.length; i++) {
     var pesoEhValido = true;
     var alturaEhValida = true;
 
-    if (peso <= 0 || peso >= 1000) {
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
-    }
+  if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
+};
 
-    if (altura <= 0 || altura >= 3.00) {
-        alturaEhValida = false;
-        tdImc.textContent = "Altura inválida";
-    }
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
+};
 
   if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
-    }
+    };
 }
 
 titulo.addEventListener("click", mostraMensagem)
